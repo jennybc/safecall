277453d62c4789761b01f2974c267bdfbb10ecbe
Gábor Csárdi <csardi.gabor@gmail.com>
2019-03-30 17:51:46 +0000

    Prototype for passing a function to a .Call

--- R/package.R
+++ R/package.R
@@ -8,10 +8,15 @@
 #' @useDynLib safecall,.registration = TRUE
 NULL
 
+
 cleanup_test <- function() {
-  .Call(xwrapper1)
+  .Call(xwrapper1, "foo", 1:10)
 }
 
 cleanup_test2 <- function() {
-  .Call(xwrapper2)
+  .Call(xwrapper2, "foo", 1:10)
+}
+
+safecall <- function(cfun, ...) {
+  .Call(c_safecall, cfun$address, list(...))
 }
